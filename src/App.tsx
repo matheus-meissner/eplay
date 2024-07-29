b601f80f1e2df7cfbb4075674fc7c50ff64b42fb
Matheus Meissner <matheus.iembo@gmail.com>
2024-07-29 07:37:09 -0300

    finaliza header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { GlobalCss } from './styles'
+import { GlobalCss, Container } from './styles'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
       <GlobalCss />
-      <div>olá mundo</div>
+      <Container>
+        <Header />
+      </Container>
     </>
   )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
-const cores = {
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const cores = {
   branca: '#eeeeee',
   preta: '#111111',
   cinza: '#333333',
@@ -12,10 +13,18 @@ export const GlobalCss = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif, sans-serif;
+    list-style: none;
   }
 
   body {
     background-color: ${cores.preta};
     color: ${cores.branca};
+    padding-top: 40px;
   }
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 `
