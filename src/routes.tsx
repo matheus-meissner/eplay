6bed41a4e8b6493979184fddd5489d6fc4d83489
Matheus Meissner <matheus.iembo@gmail.com>
2024-08-05 13:12:05 -0300

    ajusta Route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import Home from './pages/Home'
 import Categories from './pages/Categories'
+import Product from './pages/Product'
 import { Route, Routes } from 'react-router-dom'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 )
 
