4232844b27aa528c3346e11d664ce66da14ba08f
Matheus Meissner <matheus.iembo@gmail.com>
2024-08-13 12:13:30 -0300

    configura o Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalCss } from './styles'
 import Header from './components/Header'
 import Rotas from './routes'
 import Footer from './components/Footer'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <div className="container">
-        <Header />
-      </div>
-      <Rotas />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <div className="container">
+          <Header />
+        </div>
+        <Rotas />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
