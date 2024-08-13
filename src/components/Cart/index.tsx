7455c577d232ac2f433705f7d2c1670f2976de23
Matheus Meissner <matheus.iembo@gmail.com>
2024-08-13 18:14:10 -0300

    Cria o Carrinho

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,50 @@
+import Button from '../Button'
+import {
+  Overlay,
+  CartContainer,
+  Sidebar,
+  Prices,
+  Quantity,
+  CartItem
+} from './styles'
+import starwars from '../../assets/images/star_wars.png'
+import Tag from '../Tag'
+
+const Cart = () => (
+  <CartContainer>
+    <Overlay />
+    <Sidebar>
+      <ul>
+        <CartItem>
+          <img src={starwars}></img>
+          <div>
+            <h3>Nome do jogo</h3>
+            <Tag>RPG</Tag>
+            <Tag>PS5</Tag>
+            <span>R$: 150,00</span>
+          </div>
+          <button type="button" />
+        </CartItem>
+        <CartItem>
+          <img src={starwars}></img>
+          <div>
+            <h3>Nome do jogo</h3>
+            <Tag>RPG</Tag>
+            <Tag>PS5</Tag>
+            <span>R$: 150,00</span>
+          </div>
+          <button type="button" />
+        </CartItem>
+      </ul>
+      <Quantity>2 jogos(s) no carrinho</Quantity>
+      <Prices>
+        Total de R$: 250,00 <span>Em até 6x vez juros</span>
+      </Prices>
+      <Button title="Clique aqui para continuar com a compra" type="button">
+        Continar com a compra
+      </Button>
+    </Sidebar>
+  </CartContainer>
+)
+
+export default Cart
